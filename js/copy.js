df5d3b3e732e2f108a3ec9f69e3e5fb36d7efdd3
Diego Paixão <diego.spaixao91@hotmail.com>
2023-06-03 00:56:03 -0300

    Melhorias no código e no design
    - Refatoração do código.
    - Atualização do estilo do formulário de pesquisa e da seção de resultados.
    - Ajuste do layout da seção do formulário para separar o título e os elementos de pesquisa.
    - Correção do problema de alinhamento na seção do formulário para garantir um espaçamento adequado entre os elementos.
    - Exibição das estatísticas do Pokémon (HP, altura e peso) em um layout em linha para melhor legibilidade.
    - Melhoria na exibição da cadeia de evolução, utilizando um contêiner e elementos individuais para cada evolução.
    - Atualização da exibição da descrição do Pokémon para mostrar uma mensagem padrão quando não há descrição disponível.

--- js/copy.js
+++
@@ -1,6 +0,0 @@
-const currentYear = new Date().getFullYear();
-const copyrightElement = document.querySelector('#copyright');
-const appTitle = 'PokeGuide';
-
-copyrightElement.innerHTML =
-  `&copy; ${currentYear} ${appTitle}`;
--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+// Selecionando os elementos HTML
 const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-id');
 const pokemonImage = document.querySelector('.pokemon-img');
 const pokemonEvolution = document.querySelector('.pokemon-evolution');
 const pokemonAbilitiesList = document.querySelector('.pokemon-abilities');
 const pokemonTypesList = document.querySelector('.pokemon-types');
+const pokemonDescription = document.querySelector('.pokemon-description');
+const pokemonStats = document.querySelector('.pokemon-stats');
+const pokemonHP = document.querySelector('.pokemon-hp');
+const pokemonHeight = document.querySelector('.pokemon-height');
+const pokemonWeight = document.querySelector('.pokemon-weight');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
@@ -12,6 +18,7 @@ const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
 
+// Função para buscar os dados do Pokémon na API
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
@@ -21,19 +28,51 @@ const fetchPokemon = async (pokemon) => {
   }
 }
 
+// Função para buscar a evolução do Pokémon na API
 const fetchEvolution = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
-  
-    if (APIResponse.status === 200) {
-      const data = await APIResponse.json();
-      const evolutionChain = await fetch(data.evolution_chain.url);
-      const evolutionData = await evolutionChain.json();
-      return evolutionData;
-    }
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    const evolutionChain = await fetch(data.evolution_chain.url);
+    const evolutionData = await evolutionChain.json();
+    return evolutionData;
   }
+}
 
+// Função para buscar a descrição do Pokémon na API
+const fetchPokemonDescription = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    const description = data.flavor_text_entries.find(entry => entry.language.name === 'en');
+    return description.flavor_text;
+  }
+}
+
+// Função para buscar o ID do Pokémon na API
+const fetchPokemonId = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data.id;
+  }
+}
+
+// Função para buscar o sprite do Pokémon na API
+const fetchPokemonSprite = async (pokemonId) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data.sprites.front_default;
+  }
+}
+
+// Função para renderizar os dados do Pokémon na página
 const renderPokemon = async (pokemon) => {
-
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = '';
 
@@ -43,23 +82,22 @@ const renderPokemon = async (pokemon) => {
     pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    pokemonImage.src = data.sprites.other['official-artwork'].front_default;
 
     pokemonTypesList.innerHTML = '';
     if (data['types'].length > 1) {
       const types = data['types'];
       const typesList = document.createElement('ul');
-    
+
       types.forEach((type) => {
         const typeItem = document.createElement('li');
         typeItem.textContent = type['type']['name'];
         typesList.appendChild(typeItem);
       });
-    
+
       pokemonTypesList.appendChild(typesList);
     } else {
       const types = data['types'];
-            
       pokemonTypesList.innerHTML = `
         <li>${types[0]['type']['name']}</li>
       `;
@@ -67,39 +105,75 @@ const renderPokemon = async (pokemon) => {
 
     pokemonAbilitiesList.innerHTML = '';
     if (data['abilities'].length > 1) {
-        const abilities = data['abilities'];
-        const abilitiesList = document.createElement('ul');
-        
-        abilities.forEach((ability) => {
-          const abilityItem = document.createElement('li');
-          abilityItem.textContent = ability['ability']['name'];
-          abilitiesList.appendChild(abilityItem);
-        });
-        
-        pokemonAbilitiesList.appendChild(abilitiesList);
-      } else {
-        const pokemonAbilities = data['abilities'];
-        
-        pokemonAbilitiesList.innerHTML = `
-          <li>${pokemonAbilities[0]['ability']['name']}</li>
-        `;
-      }
+      const abilities = data['abilities'];
+      const abilitiesList = document.createElement('ul');
+
+      abilities.forEach((ability) => {
+        const abilityItem = document.createElement('li');
+        abilityItem.textContent = ability['ability']['name'];
+        abilitiesList.appendChild(abilityItem);
+      });
+
+      pokemonAbilitiesList.appendChild(abilitiesList);
+    } else {
+      const pokemonAbilities = data['abilities'];
+
+      pokemonAbilitiesList.innerHTML = `
+        <li>${pokemonAbilities[0]['ability']['name']}</li>
+      `;
+    }
+
     input.value = '';
     searchPokemon = data.id;
 
     const evolutionData = await fetchEvolution(data.id);
     if (evolutionData) {
-        let evolutions = [];
-        let currentEvolution = evolutionData.chain;
-        while (currentEvolution) {
-          evolutions.push(currentEvolution.species.name);
-          currentEvolution = currentEvolution['evolves_to'][0];
-        }
-        pokemonEvolution.innerHTML = evolutions.join(' > ');
-      } else {
-        pokemonEvolution.innerHTML = 'No evolution data available.';
+      let evolutions = [];
+      let currentEvolution = evolutionData.chain;
+      while (currentEvolution) {
+        const evolutionId = await fetchPokemonId(currentEvolution.species.name);
+        const evolutionSprite = await fetchPokemonSprite(evolutionId);
+        const evolutionName = currentEvolution.species.name;
+
+        const evolutionContainer = document.createElement('div');
+        evolutionContainer.classList.add('evolution-item');
+
+        const evolutionImg = document.createElement('img');
+        evolutionImg.src = evolutionSprite;
+        evolutionImg.alt = evolutionName;
+
+        const evolutionNameElement = document.createElement('span');
+        evolutionNameElement.textContent = evolutionName;
+
+        evolutionContainer.appendChild(evolutionImg);
+        evolutionContainer.appendChild(evolutionNameElement);
+
+        evolutions.push(evolutionContainer.outerHTML);
+        currentEvolution = currentEvolution['evolves_to'][0];
       }
-     
+
+      pokemonEvolution.innerHTML = '';
+      const evolutionWrapper = document.createElement('div');
+      evolutionWrapper.classList.add('evolution-wrapper');
+      evolutionWrapper.innerHTML = evolutions.join(' ');
+      pokemonEvolution.appendChild(evolutionWrapper);
+    } else {
+      pokemonEvolution.innerHTML = 'No evolution data available.';
+    }
+
+    const description = await fetchPokemonDescription(data.name);
+    if (description) {
+      pokemonDescription.textContent = description;
+    } else {
+      pokemonDescription.textContent = 'No description available.';
+    }
+
+    // Exibir as informações de HP, Height e Weight
+    pokemonStats.style.display = 'block';
+    pokemonHP.textContent = data.stats.find(stat => stat.stat.name === 'hp').base_stat;
+    pokemonHeight.textContent = data.height;
+    pokemonWeight.textContent = data.weight;
+
   } else {
     pokemonImage.style.display = 'none';
     pokemonName.innerHTML = 'Not found :c';
@@ -107,11 +181,13 @@ const renderPokemon = async (pokemon) => {
   }
 }
 
+// Função para lidar com o envio do formulário de pesquisa
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   renderPokemon(input.value.toLowerCase());
 });
 
+// Função para lidar com o clique no botão "Prev"
 buttonPrev.addEventListener('click', () => {
   if (searchPokemon > 1) {
     searchPokemon -= 1;
@@ -119,9 +195,11 @@ buttonPrev.addEventListener('click', () => {
   }
 });
 
+// Função para lidar com o clique no botão "Next"
 buttonNext.addEventListener('click', () => {
   searchPokemon += 1;
   renderPokemon(searchPokemon);
 });
 
+// Renderizar o Pokémon inicial ao carregar a página
 renderPokemon(searchPokemon);
